4b6332897893c3caa015ff36292780ed82fbbf7f
carolineolagunju <olamidejr21@gmail.com>
2023-06-19 04:15:19 -0600

    refactored and tested function word search

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,8 +31,60 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
+  });
+
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'S', 'A', 'L'],
+      ['X', 'X', 'X', 'X', 'X', 'E', 'X', 'X'],
+      ['Y', 'F', 'C', 'F', 'Q', 'I', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'N', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'F', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'L', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'D', 'A', 'S'],
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+
+  it("should return false if the array contains empty arrays", function() {
+    const result = wordSearch([
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+    ], 'SEINFELD');
+
+    assert.isFalse(result);
+  });
+
+
+  it("should return false if the array is empty", function() {
+    const result = wordSearch([], 'SEINFELD');
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if a string is passed as a paramater", function() {
+    const result = wordSearch("testing!", 'SEINFELD');
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if a number is passed as a paramater", function() {
+    const result = wordSearch(123456, 'SEINFELD');
+
+    assert.isFalse(result);
   });
 });
